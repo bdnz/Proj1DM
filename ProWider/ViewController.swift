0a48528ffdafbd5209be3299a56cfa533f30c286
Deniz <Deniz@Macs-MacBook-Pro.local>
2019-04-15 12:04:08 -0600

    UI tryout FirstCommit

---
+++ ProWider/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ProWider
+//
+//  Created by Deniz on 4/11/19.
+//  Copyright © 2019 Deniz. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class ViewController: UIViewController {
+
+    @IBAction func ProviderBTN(_ sender: Any) {
+    }
+    
+    @IBAction func CustomerBTN(_ sender: Any) {
+    }
+    
+    @IBAction func RegisteredBTN(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
